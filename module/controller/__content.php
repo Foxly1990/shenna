cd4a3ab3b61b90b93a74609e0b7943e2fba79d7b
Ruslan Berebnev <flokistable@protonmail.com>
2016-07-23 20:57:42 +0300

    Renew...

--- module/controller/__content.php
+++
@@ -1,5 +0,0 @@
-<?php
-include $_SERVER["DOCUMENT_ROOT"]."/shenna/module/content.php";
-$content = new Content;
-$arrContent = $content->getContent("new_ru");
-?>
--- module/controller/__writer.php
+++
@@ -1,7 +0,0 @@
-<?php
-include $_SERVER["DOCUMENT_ROOT"]."/shenna/module/content.php";
-$content = new Content;
-$setContent = $content->setContent("new_ru");
-
-echo json_encode($setContent);
-?>
